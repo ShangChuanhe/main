59a6399ebb7c8e38abf431d0f3d553a6b0d15e1d
ShangChuanhe <15665408891@163.com>
2020-09-17 18:47:13 +0800

    v.1 the initial version

---
+++ examples/main.cpp
@@ -0,0 +1,44 @@
+/*
+	    Welcome to my world!!
+	         Just do it
+                       ------------The final right of interpretation belongs to Mr. Shang
+*/
+
+#include <mockturtle/mockturtle.hpp>
+#include <alice/alice.hpp>
+#include "store.hpp"
+
+//#include <stdio.h>
+
+/*
+namespace alice
+{
+
+
+}
+*/
+ALICE_MAIN(main)
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int main(int argc, char** argv)
+{
+    double a = 49.0; 
+    double b = 0.0;
+ 
+    printf("input a:%f\n",a);
+//    b = cal_sqrt(a);
+    printf("sqrt result:%f\n",b);
+    return 0;
+}
+*/
